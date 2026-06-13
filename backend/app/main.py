d145b6558ae3116c68c41f923599c363d0dcd321
octoetix <iam8nd9@gmail.com>
2026-06-13 14:17:47 +0100

    fix: add missing CORS origin for deployed application

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://buildlabs-capstone-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
